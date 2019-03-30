019fe01b218f9b8aedea48cb6fbaa88bef502867
Saurav <sauravpanda84@gmail.com>
2019-03-30 12:51:34 +0530

    landing hero bar and navigation init

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,41 +1,78 @@
 import React, { Component } from 'react';
 import './Header.scss';
 import { NavLink } from 'react-router-dom'
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import Logo from '../../assets/images/logo.svg';
 
 class Header extends Component {
+
+  state = {
+    anchorEl: null,
+  };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+
   render() {
+
+    const { anchorEl } = this.state;
+
     return (
       <div className="navigation">
-        
-      <div className="navTabsBar">
-          <nav className="navTabs">
-              <ul className="navTabs-list list-inline">
-                  <li className="navTabs-item">
-                      <NavLink to="/about" activeClassName="is-active">
-                        About
-                      </NavLink>
-                  </li>
-                  <li className="navTabs-item">
-                    <NavLink to="/experience" activeClassName="is-active">
-                        Experience
-                    </NavLink>
-                  </li>
-                  <li className="navTabs-item">
-                    <NavLink to="/work" activeClassName="is-active">
-                        Work
-                    </NavLink> 
-                  </li>
-                  <li className="navTabs-item">
-                    <NavLink to="/contact" activeClassName="is-active">
-                        Contact
-                    </NavLink>
-                  </li>
-              </ul>
-          </nav>
-      </div>
+        <div>
+           <img style={{height: 80}} src={Logo}/>
+        </div>
+        <div className="navTabsBar">
+            <nav className="navTabs">
+                <ul className="navTabs-list list-inline">
+                    <li className="navTabs-item">
+                        <NavLink to="/" activeClassName="is-active">
+                          Home
+                        </NavLink>
+                    </li>
+
+                    <li className="navTabs-item">
+                      <Button
+                        aria-owns={anchorEl ? 'simple-menu' : undefined}
+                        aria-haspopup="true"
+                        onClick={this.handleClick}
+                        style={{
+                          color: '#682F92'
+                        }}
+                      >
+                        Features
+                      </Button>
+                      <Menu
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={this.handleClose}
+                      >
+                        <MenuItem onClick={this.handleClose}>Featire A</MenuItem>
+                      </Menu>
+                    </li>
+
+                    <li className="navTabs-item">
+                      <NavLink to="/pricing" activeClassName="is-active">
+                          Pricing
+                      </NavLink> 
+                    </li>
+              
+                </ul>
+            </nav>
+        </div>
+
     </div>
     );
-  }
+ }
 }
 
 export default Header;
--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import './Landing.scss';
 import { Link } from "react-router-dom";
-
+import Header from '../../components/header/Header';
 import { siteData } from '../../constant';
 
 class Landing extends Component {
   render() {
     return (
       <section className="landing">
-        <h1>Landing</h1>
-        <h2>Checking automated dep</h2>
+        <Header/>
+        <div className="hero">
+          <div className="content">
+              <div>
+                <h1>Ready for virtual classrooms?</h1>
+                <h3>Sign up to get virtual classrooms for your school / colleges</h3>
+              </div>
+              <div>
+                <img src="https://kiwihr.com/static/main.54b30db9.png" />
+              </div>
+          </div>
+        </div>
       </section>
     )
   }

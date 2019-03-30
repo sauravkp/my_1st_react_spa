dd1e41bce325c956fe43143801a98bfddf156453
Saurav <sauravpanda84@gmail.com>
2019-03-30 13:37:50 +0530

    landing page card created

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -3,6 +3,7 @@ import './Landing.scss';
 import { Link } from "react-router-dom";
 import Header from '../../components/header/Header';
 import { siteData } from '../../constant';
+import Icon from '@material-ui/core/Icon';
 
 class Landing extends Component {
   render() {
@@ -18,6 +19,70 @@ class Landing extends Component {
               <div>
                 <img src="https://kiwihr.com/static/main.54b30db9.png" />
               </div>
+          </div>
+        </div>
+
+        <div className="features">
+          <h1 className="headline">Ready for virtual classrooms?</h1>
+          <div className="is-row">
+            <div className="is-col-md-4">
+                <div className="card">
+                  <div className="icon">
+                    <Icon>star</Icon>
+                  </div>
+                  <h3>Lorem Ipsum</h3>
+                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                </div>
+            </div>
+
+            <div className="is-col-md-4">
+                <div className="card">
+                  <div className="icon">
+                    <Icon>star</Icon>
+                  </div>
+                  <h3>Lorem Ipsum</h3>
+                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                </div>
+            </div>
+
+            <div className="is-col-md-4">
+                <div className="card">
+                  <div className="icon">
+                    <Icon>star</Icon>
+                  </div>
+                  <h3>Lorem Ipsum</h3>
+                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                </div>
+            </div>
+
+            <div className="is-col-md-4">
+                <div className="card">
+                  <div className="icon">
+                    <Icon>star</Icon>
+                  </div>
+                  <h3>Lorem Ipsum</h3>
+                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                </div>
+            </div>
+            <div className="is-col-md-4">
+                <div className="card">
+                  <div className="icon">
+                    <Icon>star</Icon>
+                  </div>
+                  <h3>Lorem Ipsum</h3>
+                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                </div>
+            </div>
+            <div className="is-col-md-4">
+                <div className="card">
+                  <div className="icon">
+                    <Icon>star</Icon>
+                  </div>
+                  <h3>Lorem Ipsum</h3>
+                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                </div>
+            </div>
+            
           </div>
         </div>
       </section>

a5e624509d668c611a41f656241f62797e02e23b
Saurav <sauravpanda84@gmail.com>
2019-04-02 18:44:09 +0530

    small ui changes

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -14,8 +14,8 @@ class Landing extends Component {
         <div className="hero">
           <div className="content">
               <div>
-                <h1>Ready for virtual classrooms?</h1>
-                <h3>Sign up to get virtual classrooms for your school / colleges</h3>
+                <h1>Simply efficient Virtual Classrooms </h1>
+                <p>CentEdge helps schools, colleges and tuition centers set-up virtual classrooms with live streaming and recording without any extra hardware cost.</p>
               </div>
               <div>
                 <img height="400" src="https://epecate.com/wp-content/uploads/2019/01/virtual_classroom3_imac2015retina_front-1024x1024.png" />
@@ -24,15 +24,25 @@ class Landing extends Component {
         </div>
 
         <div className="features">
-          <h1 className="headline">Ready for virtual classrooms?</h1>
+          <h1 className="headline">Next generation Teaching Learning with CentEdge</h1>
           <div className="is-row">
             <div className="is-col-md-4">
                 <div className="card">
                   <div className="icon">
                     <Icon>star</Icon>
                   </div>
-                  <h3>Lorem Ipsum</h3>
-                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                  <h3>Anytime anywhere Teaching</h3>
+                  <p>Live stream your class directly from your virtual classroom anytime anywhere with exactly 2 clicks. Any student can raise hand to ask doubt through voice and teacher can choose whose request to accept.</p>
+                </div>
+            </div>
+            
+            <div className="is-col-md-4">
+                <div className="card">
+                  <div className="icon">
+                    <Icon>star</Icon>
+                  </div>
+                  <h3>Live Streaming with recording</h3>
+                  <p>The virtual classrooms comes with interactive live streaming and recording as default features with different pre-configured options for educational institute and training centers. It can help them in reaching and teaching more students with their existing resources.</p>
                 </div>
             </div>
 
@@ -41,8 +51,8 @@ class Landing extends Component {
                   <div className="icon">
                     <Icon>star</Icon>
                   </div>
-                  <h3>Lorem Ipsum</h3>
-                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                  <h3>Online quiz, assignment and feedback</h3>
+                  <p> The virtual classrooms are equipped with online, quiz, assignments to measure the students understanding. The real time fedback by students helps the teacher in understanding the teaching quality and effectiveness. </p>
                 </div>
             </div>
 
@@ -51,8 +61,8 @@ class Landing extends Component {
                   <div className="icon">
                     <Icon>star</Icon>
                   </div>
-                  <h3>Lorem Ipsum</h3>
-                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                  <h3>Integrated Management</h3>
+                  <p>Builtin admin dashboards along with on-demand integration with other services like HRMS, ERP, LMS, Accreditation, Biometric etc.</p>
                 </div>
             </div>
 
@@ -61,8 +71,8 @@ class Landing extends Component {
                   <div className="icon">
                     <Icon>star</Icon>
                   </div>
-                  <h3>Lorem Ipsum</h3>
-                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                  <h3>Share, Re-use &amp; Collaborate</h3>
+                  <p>The recording can be re-used by sharing it inside a virtual classrooms or can even be downloaded only by the author. The future students can get benefited from the recordings by going through some of the recordings even before the actual class begins. </p>
                 </div>
             </div>
             <div className="is-col-md-4">
@@ -70,17 +80,8 @@ class Landing extends Component {
                   <div className="icon">
                     <Icon>star</Icon>
                   </div>
-                  <h3>Lorem Ipsum</h3>
-                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
-                </div>
-            </div>
-            <div className="is-col-md-4">
-                <div className="card">
-                  <div className="icon">
-                    <Icon>star</Icon>
-                  </div>
-                  <h3>Lorem Ipsum</h3>
-                  <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</p>
+                  <h3>Skill gap analysis</h3>
+                  <p>Academic analytics, behavioural analytics, marks and attendance analytics, video analytics etc for course content improvement, student engagement improvement, student academic performance improvement.</p>
                 </div>
             </div>
             
